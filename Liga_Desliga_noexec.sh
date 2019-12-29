c182d346b84a76462f73bdbc05f455ccc06db8d5
Pinheiro95 <32178843+Pinheiro95@users.noreply.github.com>
2019-12-29 17:57:40 -0300

    Add files via upload

---
+++ Liga_Desliga_noexec.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------#
+#Script Name: Liga_Desliga_noexec.sh
+#Description: Habilita e desabilita a opção de montagem da partição noexec para permitir
+#que programas sejam instalados corretamente no seu ambiente GNU/Linux
+#Linkedin: https://www.linkedin.com/in/bruno-pinheiro-502796152
+#Referencia: Hardening em Linux - Sandro Melo - RNP
+#Escrito por: Bruno Pinheiro
+#Manutenção: Bruno Pinheiro
+#-------------------------------------------------------------------#
+#Comando:
+#	$ ./Liga_Desliga_noexec.sh
+#-------------------------------------------------------------------#
+#Versão Bash
+#	5.0.3
+#-------------------------------------------------------------------#
+#Historico
+#	v1.0 29/12/2019, Bruno:
+#	-Inicio do programa
+#-------------------------------------------------------------------#
+#Obs:
+#	Execute o script como root para aplicar as opcoes
+#-------------------------------------------------------------------#
+
+#!/bin/bash
+
+case $1 in
+ 	start)
+ 		mount -o remount,rw,noexec /var
+ 		mount -o remount,rw,noexec /tmp
+	mount
+ 	echo “Partições SEM permissão de execução”
+;;
+ 	stop)
+ 		mount -o remount,rw,exec /var
+ 		mount -o remount,rw,exec /tmp
+	mount
+ 	echo “Partições COM permissão de execução”
+;;
+ 	*) echo “erro use $0 {start|stop}”
+	exit 0
+;;
+esac
+exit 1
