cb0a2986e4b451ee63543f727041962a2afde255
Pinheiro95 <32178843+Pinheiro95@users.noreply.github.com>
2020-01-11 08:21:49 -0300

    Add files via upload
    Script feito com a intenção de automatizar o processo de auditoria de segurança em servidores GNU/Linux.
    Ferramentas utilizadas:
                                        - Debsecan
                                        - Rkrootkit
                                        - Chkrootkit
    Os resultados da execução do script são encontrados em /var/log/aditoriaSecOps

---
+++ AudSecOps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------#
+#Script Name: AudSecOps.sh
+#Description: Executar auditoria de segurança em sistemas Debian com debsecan, chkrootkit e rkhunter
+#Script escrito e testado no Debian 10 Buster
+#Linkedin: https://www.linkedin.com/in/bruno-pinheiro-502796152
+#Referencia: Hardening em Linux - Sandro Melo - RNP
+#Escrito por: Bruno Pinheiro
+#Manutenção: Bruno Pinheiro
+#-------------------------------------------------------------------#
+#Comando:
+#	$ ./AudSecOps.sh
+#-------------------------------------------------------------------#
+#Versão Bash
+#	5.0.3
+#-------------------------------------------------------------------#
+#Historico
+#	v1.0 09/01/2020, Bruno:
+#	-Inicio do programa
+#-------------------------------------------------------------------#
+#Obs:
+#	Execute o script como root para aplicar as opcoes
+#       É necessário ter os seguintes programas instalados previamente:
+#	debsecan, rkhunter, chkrootkit
+#-------------------------------------------------------------------#
+
+#cria diretorio que contem os arquivos criados pelo script
+mkdir /var/log/auditoriaSecOps
+cd /audtoriaSecOps
+
+#auditoria com debsecan. Primeiro criando um arquivo reportando as vulnerabilidades encontradas
+#depois atualiza os pacotes com brechas de segurança baseada na lista de vulnerabilidades do Mitre
+# Do CVE https://cve.mitre.org/index.html
+debsecan --format report > auditoria.txt
+debsecan --format packages > AtualizaPacotes.txt
+apt-get install $(cat AtualizaPacotes.txt)
+
+#Procura por rootkit
+rkhunter --cronjob --rwo > RkHunterResults.txt
+chkrootkit > chkrootkitResults.txt
+
+echo "Auditoria finalizada. Verifique os arquivos presentes na em /var/log/AuditoriaSecOps"
+echo "auditoria.txt - vulnerabilidades encontradas (consulte o CVE)"
+echo "AtualizaPacotes.txt - Pacotes que foram atualizados"
+echo "RkhunterResults.txt - Resultado do buscador de rootkit"
+echo "chkrootkitResults.txt - Resultado do segundo buscador de rootkit"
+
+exit 0
